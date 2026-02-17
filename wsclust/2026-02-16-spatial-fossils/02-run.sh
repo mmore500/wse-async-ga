9ac7708a9e821df94772a635e344bb052ef78941
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-02-16 22:56:04 -0500

    Bugfix: rename purifying{plus,sweep}

--- wsclust/2026-02-16-spatial-fossils/02-run.sh
+++ wsclust/2026-02-16-spatial-fossils/02-run.sh
@@ -142,7 +142,7 @@ echo ">>>>> ${FLOWNAME} :: ${STEPNAME} || ${SECONDS}"
 ###############################################################################
 # lex12 configuration: two genome flavors with seeds 1 and 2
 seed=0
-for genome_flavor in genome_purifyingonly genome_purifyingplus; do
+for genome_flavor in genome_purifyingonly genome_purifyingsweep; do
     seed=$((seed + 1))
     CONFIG_NAME="flavor=${genome_flavor}+seed=${seed}"
     echo
